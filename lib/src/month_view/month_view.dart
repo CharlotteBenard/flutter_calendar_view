ffa2ea8efc3fafa379a5b0f68413ab7f305b4f34
Charlotte Bénard <charlottebenard@isipharm.fr>
2023-05-10 16:48:47 +0200

    feat: update width to fit padding

--- lib/src/month_view/month_view.dart
+++ lib/src/month_view/month_view.dart
@@ -382,7 +382,7 @@ class MonthViewState<T extends Object?> extends State<MonthView<T>> {
   }
 
   void updateViewDimensions() {
-    _width = widget.width ?? MediaQuery.of(context).size.width;
+    _width = widget.width ?? MediaQuery.of(context).size.width - 30;
     _cellWidth = _width / 7;
     _cellHeight = _cellWidth / widget.cellAspectRatio;
     _height = _cellHeight * 5;
